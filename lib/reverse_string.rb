d30b2c946e3b6cad0cfed7c852de9f36a9f89d3e
maedae <meagan.jungman@gmail.com>
2016-01-06 20:49:14 +0000

    done

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,5 +1,16 @@
 def reverse_characters(str)
+    sample_string = "Today is a good day"
+    sample_string = sample_string.split("")
+    revphrase = []
+    revphrase.push(sample_string.pop) until sample_string.empty?
+    sample_string = revphrase
+    sample_string = sample_string.join("")
 end
-
 def reverse_words(str)
+    sample_string = "Today is a good day"
+    sample_string = sample_string.split(" ")
+    revphrase = [ ]
+    revphrase.push(sample_string.pop) until sample_string.empty?
+    sample_string = revphrase
+    sample_string = sample_string.join(" ")
 end
